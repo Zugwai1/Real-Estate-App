c44ab455317d934f80634d0718c912db503ebcf0
laken11 <olaniteolalekan@gmail.com>
2022-09-09 17:33:44 +0100

    fixed issues in js redirection and fields

--- web/static/js/app/signup.js
+++ web/static/js/app/signup.js
@@ -53,6 +53,7 @@ document.getElementById('registration-form').addEventListener('submit', async (e
     let isValid = document.getElementById('registration-form').checkValidity() ? [true, "Form validated"] : [false, "Please ensure you fill all forms correctly"]
     let isPasswordValid =
         validatePassword(document.getElementById('password'), document.getElementById('confirm_password')) ? [true, "Password validated"] : [false, "Password dose not match"]
+
     let data = new FormData(e.target)
     if (isValid[0] && isPasswordValid[0]) {
         let response = await fetch(`${baseUrl}api/v1/users/`, {
